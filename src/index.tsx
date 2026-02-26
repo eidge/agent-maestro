82714c911786c15ec6d11538e25e21387a88d771
Hugo Ribeira <hribeira@figma.com>
2026-02-26 17:41:18 +0000

    Add OpenTUI skill

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <box alignItems="center" justifyContent="center" flexGrow={1}>
       <box justifyContent="center" alignItems="flex-end">
-        <ascii-font font="tiny" text="OpenTUI" />
-        <text attributes={TextAttributes.DIM}>What will you build?</text>
+        <ascii-font font="tiny" text="Agent-Maestro" />
+        <text attributes={TextAttributes.DIM}>Keep your agent orchestra in check.</text>
       </box>
     </box>
   );
